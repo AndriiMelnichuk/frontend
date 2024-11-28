70e804eb228cc93852d2315aec488c16b2879b87
AndriiMelnichuk <andrii.melnichuk@nure.ua>
2024-11-28 19:49:58 +0200

    fix: change debug mode

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ app = create_app()
 app.config["SECRET_KEY"] = 'your_secret_key'
 # RabbitMQProducer()
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
