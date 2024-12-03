3f25588a2f6c351dfa227d152b499fa24839f4e2
AndriiMelnichuk <andrii.melnichuk@nure.ua>
2024-12-03 15:17:24 +0200

    feat: update task in calendar

--- app/routes/calendar.py
+++ app/routes/calendar.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, jsonify
-
+import json
 from app.utils import Validator, InternetTalker
 
 calendar = Blueprint('calendar', __name__)
@@ -22,3 +22,22 @@ def getAllAssignedTaskRoute(dateString):
                      'status': t.status,
                      'date': t.date} for t in tasks])
     
+@calendar.route('/task/update/')
+def updateTask():
+    id = request.args.get('id')
+    data = request.args.get('data')
+    data = json.loads(data)
+    InternetTalker.updateTask(id, data)
+    return redirect('/calendar')
+
+
+@calendar.route('/task/complete/<taskId>/<groupId>')
+def completeTask(taskId, groupId):
+    InternetTalker.completeTask(taskId, groupId)
+    return redirect('/calendar')
+
+
+@calendar.route('/task/delete/<taskId>/<groupId>')
+def deleteTask(taskId, groupId):
+    InternetTalker.deleteTask(groupId, taskId)
+    return redirect('/calendar')
